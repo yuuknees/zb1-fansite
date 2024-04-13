0b2167a5ef78b8330af8894fe5a502542a9224a9
yuuknees <79818275+yuuknees@users.noreply.github.com>
2024-04-13 13:59:41 +1000

    javascript

--- script.js
+++ script.js
@@ -15,19 +15,3 @@ cancelButton.addEventListener("click", () => {
     dialog.close("animalNotChosen");
     toggleDarkBackground(); // Toggle dark background when the dialog closes
 });
-
-const updateButtonHanbin = document.getElementById("quickStats_hanbin"); // Change the ID for the quickStats button
-const cancelButtonHanbin = document.getElementById("cancel_hanbin"); // Change the ID for the cancel button
-const dialogHanbin = document.getElementById("favDialog_hanbin"); // Change the ID for the dialog box
-
-// Update button opens a modal dialog
-updateButtonHanbin.addEventListener("click", () => {
-    dialogHanbin.showModal();
-    toggleDarkBackground(); // Toggle dark background when the dialog opens
-});
-
-// Form cancel button closes the dialog box
-cancelButtonHanbin.addEventListener("click", () => {
-    dialogHanbin.close("animalNotChosen");
-    toggleDarkBackground(); // Toggle dark background when the dialog closes
-});
